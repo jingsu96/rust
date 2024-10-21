8d919a1d0858393c22f8a016d4cd312643020afd
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-10-21 20:49:28 +0800

    feat: v1 - basic function

--- eval_expr/src/main.rs
+++ eval_expr/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn eval_expr(src: &str) -> Result<i32, String> {
+    Ok(3)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eval_expr() {
+        assert_eq!(eval_expr("3"), Ok(3));
+    }
 }
