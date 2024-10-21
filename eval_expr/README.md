f55fa5bc3338b259fe0e5c40593015666603b5d4
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-10-21 20:53:39 +0800

    feat: v2 - tokenize input

--- eval_expr/README.md
+++ eval_expr/README.md
@@ -1,3 +1,6 @@
 # eval_expr
 
 This is a simple expression evaluator written in rust. It can evaluate simple arithmetic expressions.
+
+- v1: Simple functionality.
+- v2: This version includes a Token enum for defining units, a tokenize function to convert input strings into tokens, an `eval_expr` function to parse tokens and return tokens.
--- eval_expr/src/main.rs
+++ eval_expr/src/main.rs
@@ -1,5 +1,49 @@
-fn eval_expr(src: &str) -> Result<i32, String> {
-    Ok(3)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Token {
+    Number(i32),
+    Plus,
+    Minus,
+}
+
+fn tokenize(src: &str) -> Result<Vec<Token>, String> {
+    let mut tokens = Vec::new();
+    let mut chars = src.chars().peekable();
+
+    while let Some(&c) = chars.peek() {
+        match c {
+            '0'..='9' => {
+                let mut num = 0;
+
+                while let Some(&c) = chars.peek() {
+                    if c.is_digit(10) {
+                        num = num * 10 + c.to_digit(10).unwrap() as i32;
+                        chars.next();
+                    } else {
+                        break;
+                    }
+                }
+                tokens.push(Token::Number(num));
+            }
+            '+' => {
+                tokens.push(Token::Plus);
+                chars.next();
+            }
+            '-' => {
+                tokens.push(Token::Minus);
+                chars.next();
+            }
+            ' ' => {
+                chars.next();
+            }
+            _ => return Err(format!("Invalid character: {}", c)),
+        }
+    }
+    Ok(tokens)
+}
+
+fn eval_expr(src: &str) -> Result<Vec<Token>, String> {
+    let tokens = tokenize(src)?;
+    Ok(tokens)
 }
 
 #[cfg(test)]
@@ -7,7 +51,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_eval_expr() {
-        assert_eq!(eval_expr("3"), Ok(3));
+    fn test_tokenize() {
+        assert_eq!(
+            eval_expr("1 + 2 - 3").unwrap(),
+            vec![
+                Token::Number(1),
+                Token::Plus,
+                Token::Number(2),
+                Token::Minus,
+                Token::Number(3)
+            ]
+        );
     }
 }
